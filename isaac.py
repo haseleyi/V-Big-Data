f3063fd05c47f28f6bec8626171acbf87a323312
Isaac Haseley <isaac.haseley@gmail.com>
2017-11-26 10:22:56 +0100

    Code for reading in courses

--- isaac.py
+++ isaac.py
@@ -1,6 +1,10 @@
 import os
+import json
+
+courses = []
 
 path = "Carleton Data - Past Terms"
 for term in os.listdir(path):
 	markdown = open(path + "\\" + term).read()
-	print markdown
+	json_term = json.loads(markdown)
+	courses.extend([course for course in json_term["course_info"]])
