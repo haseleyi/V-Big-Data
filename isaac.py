2306261a46b2e0a34d7ad2bf1e8995ac1303ac0d
Isaac Haseley <isaac.haseley@gmail.com>
2017-11-27 15:32:34 +0100

    Predicting on both departments and distros

--- isaac.py
+++ isaac.py
@@ -4,8 +4,9 @@ from sklearn.naive_bayes import BernoulliNB, GaussianNB
 import os
 import json
 import numpy as np
+import math
 
-courses, targets = [], []
+courses, targets, departments, distros = [], [], [], []
 
 def initialize_courses():
 	path = "Carleton Data - Past Terms"
@@ -15,21 +16,23 @@ def initialize_courses():
 			try:
 				# Since we're predicting on multiple variables in the same matrix,
 				# we'll want to make sure that all of our courses have all of said variables
-				is_valid = is_valid_course(course)
+				has_components = prediction_variables_present(course)
+
+				is_ai = "Argument & Inquiry Seminar" in course["requirements_met"].split("\n")
 				
 				lab_in_title = "Lab" in course["title"]
 				is_music = course["department"] == "MUSC"
 				six_credits = course["credits"][0] == "6"
 				is_lab = lab_in_title and not is_music and not six_credits
 				
-				if is_valid and not is_lab:
+				if has_components and not is_lab and not is_ai:
 					courses.append(course)
 			except:
 				pass
 	targets.extend([float(course["registered"]) / float(course["size"]) for course in courses])
 
 
-def is_valid_course(course):
+def prediction_variables_present(course):
 	for field in ["summary", "start_time", "end_time", "department", "requirements_met", "registered", "size"]:
 		try:
 			if course[field] == "n/a":
@@ -39,16 +42,21 @@ def is_valid_course(course):
 	return float(course["size"]) > 0
 
 
-def learn_departments():
-	
+def analyze():
+
+	print "\n===== Initial analysis =====\n"
+	print "Number of courses:", len(courses)
+	print "Number of courses filled to capacity:", len([t for t in targets if t >= 1])
+	print "Average enrollment (allowing targets greater than 1):", sum(targets) / len(targets), "\n\n"
+
 	# Construct sorted list of departments
 	department_set = set()
 	for course in courses:
 		if course["department"] not in department_set:
 			department_set.add(course["department"])
-	departments = sorted(department_set)
+	departments.extend(sorted(department_set))
 
-	# Analysis: Average course enrollment rate by department
+	# analyze: Average course enrollment rate by department
 	enrollment_by_dep = []
 	for dep in departments:
 		dep_targets = [targets[i] for i, course in enumerate(courses) if course["department"] == dep]
@@ -59,39 +67,58 @@ def learn_departments():
 		print dep, round(enroll_rate, 3),
 	print "\n\n"
 
-	# Construct 1D course-department matrix
-	dep_nums = {dep : i for i, dep in enumerate(departments)}
-	matrix = np.array([dep_nums[course["department"]] for course in courses]).reshape(-1, 1)
+	# Construct sorted list of distros
+	distro_set = set()
+	for course in courses:
+		for distro in course["requirements_met"].split("\n"):
+			if distro:
+				distro_set.add(distro)
+	distros.extend(sorted(distro_set))
+
+	# analyze: Average course enrollment rate by distro
+	enrollment_by_distro = []
+	for distro in distros:
+		distro_targets = [targets[i] for i, course in enumerate(courses) if distro in course["requirements_met"].split("\n")]
+		enrollment_by_distro.append((distro, sum(distro_targets) / len(distro_targets)))
+	enrollment_by_distro.sort(key=lambda x : x[1], reverse=True)
+	print "===== Average Course Enrollment by Distro =====\n"
+	for distro, enroll_rate in enrollment_by_distro:
+		print distro, round(enroll_rate, 3),
+	print "\n\n"
+
+
+def predict():
+
+	# Construct map for each variable
+	dep_nums = {dep : i + 1 for i, dep in enumerate(departments)}
+	distro_nums = {distro : i + 1 for i, distro in enumerate(distros)}
+
+	# Construct training matrix
+	matrix = np.zeros((len(courses), 5))
+	for i, course in enumerate(courses):
+		# One column for department classification
+		matrix[i][0] = dep_nums[course["department"]]
+		# Four columns for possible distros
+		course_distros = course["requirements_met"].split("\n")
+		course_distros = filter(lambda distro : distro, course_distros)
+		for j, distro in enumerate(course_distros):
+			matrix[i][1 + j] = distro_nums[distro]
+
+	# Standardize the data
 
 	# Train and predict
 	model = linear_model.LinearRegression()
 	predictions_same_data = model.fit(matrix, targets).predict(matrix)
 	
-	# These models both produce "Unknown label type" ValueErrors.
-	# I think certain models can only work with certain input, e.g. Bern is 0 or 1.
-	# I think Naive Bayes is actually the one we'd want if predicting on just departments.
-	# While regression tries to fit a line to the average enrollment rates, I think 
-	# Naive Bayes would assume independence between departments and just use those averages.
-	# model = GaussianNB()
-	# model = BernoulliNB() 
+	print "===== Regression Results =====\n"
+	# We can compare the error of different models like this
+	print "Mean error:", math.sqrt(metrics.mean_squared_error(targets, predictions_same_data)), "\n"
 	
-	# Analyze predictions
-	print "===== Regressing on Departments =====\n"
-	print "Mean squared error:", metrics.mean_squared_error(targets, predictions_same_data), "\n"
-	# Once we're using multiple variables, we can compare the error of different models like this ^
-	# Un-comment the below to see how the linear regression predicts for each department
-	# for i, course in enumerate(courses):
-	# 	print course["term"], course["title"], targets[i], predictions_same_data[i]
 
 def main():
 	
 	initialize_courses()
-
-	print "\n===== Initial Analysis =====\n"
-	print "Number of courses:", len(courses)
-	print "Number of courses filled to capacity:", len([t for t in targets if t >= 1])
-	print "Average enrollment (allowing targets greater than 1):", sum(targets) / len(targets), "\n\n"
-	
-	learn_departments()
+	analyze()
+	predict()
 	
 main()
